02ad9073e5e0a0585ac813b8cf9dbb1c18088f7f
rd568 <bobbyday3620@gmail.com>
2018-11-05 11:22:07 -0500

    ayyyyy

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,33 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    // The information (grade and days absent) need to correspond to each individual
+        // Need an array that stores the first and last name of each student
+        // Need a method that returns the grade for each student
+        // Need a method that uses the grade that returns that student's grade
+        // Need a method that takes all the grades, and averages them
+        // Need a method that returns the days absent for each student
+        // Need a method that tells the student if their grade is being affected from being absent too much
+        // Need a search engine where the program could pull up a student's information
+
+        // The array that stores the first and last name of each student
+        String fName[] = {"Tupac", "Sean", "Chris", "Sean", "Chris", "Calvin", "Marshal", "Robert", "Earl", "Eric", "Tracy", "William",
+                "Tauheed", "Artis", "Kim", "Donte", "Meliss", "Nasir", "Cornell", "Jermaine", "Sandre", "Talib", "Usher", "Clifford", "David"};
+        String lName[] = {"Shakur", "Combs", "Bridges", "Carter", "Wallace", "Broadus", "Mathers", "Van Winkle", "Simmons", "Wright", "Marrow",
+        "Roberts", "Epps", "Ivery", "Jones", "Smith", "Elliot", "Jones", "Haynes", "Denny", "Denton", "Greene", "Raymond", "Smith", "Brown"};
+        Arrays.sort(fName);
+        Arrays.sort(lName);
+
+        Student students[] = new Student[25];
+        Student students1;
+
+
+
+
+
     }
 }
---
+++ src/com/company/Student.java
@@ -0,0 +1,19 @@
+package com.company;
+
+/**
+ * Created by rd568 on 11/5/18.
+ */
+public class Student {
+    // State variables
+    public String fName = "";
+    public String lName = "";
+    public int gradePercentage;
+    public int absenses = 0;
+
+    // Constructor-> needs to pass the grade to each student
+    public Student(String name, int grade, int abs){
+
+    }
+
+
+}
